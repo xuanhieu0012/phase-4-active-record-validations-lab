41ad651e34689fdac4dbccd17459215de1e63408
Hieu <xuanhieu0012@gmail.com>
2021-10-12 23:41:57 -0500

    Completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction" ]}
+    validate :click_bait?
+
+    def click_bait?
+        array =[
+            /Won't Believe/i,
+            /Secret/i,
+            /Top \d/i,
+            /Guess/i
+          ]
+        if array.none?{|t| t.match title}
+            errors.add(:title, "it must be click bait")
+        end
+    end
 end
